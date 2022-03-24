3a2bebe6530fad77afc0c907801a879d017ea863
cecile <cecile.sajdak@gmail.com>
2022-03-24 15:41:01 +0100

    ajout session utilisateurs + injections

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 import os
+from .constantes import SECRET_KEY
 
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
 templates = os.path.join(chemin_actuel, "templates")
@@ -12,9 +14,14 @@ app = Flask(
     static_folder=statics
 )
 
+# On configure le secret
+app.config['SECRET_KEY'] = SECRET_KEY
 # On configure la base de données
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 # On initie l'extension
 db = SQLAlchemy(app)
 
-from .routes import livre, accueil
+# On met en place la gestion d'utilisateur-rice-s
+login = LoginManager(app)
+
+from . import routes
---
+++ app/constantes.py
@@ -0,0 +1,8 @@
+from warnings import warn
+
+LIVRES_PAR_PAGE = 5
+
+SECRET_KEY = "JE SUIS UN SECRET !"
+
+if SECRET_KEY == "JE SUIS UN SECRET !":
+    warn("Le secret par défaut n'a pas été changé, vous devriez le faire", Warning)
